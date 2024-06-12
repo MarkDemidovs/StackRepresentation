4efa9eeae2adf236179c0f0ab9ad18c2af4c42d5
Mark <markdemidovs@proton.me>
2024-06-12 21:01:33 +0300

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-var StackArray: string[] = [];
-
-StackArray.push("First");
-
-for (let i: number = 0; i < StackArray.length; i++) {
-    StackArray.push(i.toString());
-    console.log(StackArray[i.toString()]);
-}
